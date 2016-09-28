59c46763bcb70246a796e8bb8320f124e11ac7e5
erwinhenraat <e.henraat@gmail.com>
2016-09-28 18:35:25 +0200

    created incode explanation for variables and datatypes and also incode assignments

---
+++ voorbeeldcode/les2/Assignment.java
@@ -0,0 +1,10 @@
+public class Assignment{
+  public void showAssignment(){
+    System.out.println("Assignment 1: Jay you just completed your first assignment");
+    System.out.println("Assignment 2: create 3 variables here, one for your name, age and one for your postal code. Print the values to the commandline!");
+
+
+    System.out.println("Assignment 3: Use the Scanner datatype to allow a user to enter the values for name, age, and postal code. ");
+    System.out.println("Note: Find out about datatypes at the Application Programming Interface(API) reference https://docs.oracle.com/javase/7/docs/api/allclasses-noframe.html. Select the Scanner link.");
+  }
+}
