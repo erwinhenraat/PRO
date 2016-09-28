36fbf334f142e95068e7097951e3efa9ced8bb41
erwinhenraat <e.henraat@gmail.com>
2016-09-28 17:08:38 +0200

    changes to lesson2

--- voorbeeldcode/les2/Les2.java
+++ voorbeeldcode/les2/Les2.java
@@ -3,8 +3,30 @@ public class Les2
 {
   public static void main(String[] args)
   {
-    int leeftijd = 36;
-    String voornaam = "erwin";
-    String achternaam = "henraat";
+    //example of a primitive variable
+    int age = 36;
+    char initial = 'e';
+    float weight = 83.65312f;
+    double temperature = 37.234322342342342423423423423;
+
+    //example of a reference variable
+    String firstName = "erwin";
+    String lastName = new String("Henraat");
+
+    String camelCasing = "this means starting each word after the first with a Capital letter";
+    String noncamelcasedvariablesarehardertoread = " ";
+    String thanCamelCasedVariablesWhichAreEasier = " ";
+
+    String _underscore = "you can start a variable name with _";
+    //String 5tartingWithNumbers = "This is not allowed";
+    //String %justLikeThis = "special characters are not allowed";
+
+    //This is a linecomment, it comments a single line
+    /*
+    This is a block comment
+    it comments multiple lines
+    */
+
+
   }
 }
