543872c8a8431a8e6be69d4e3731a06aa36d6423
erwinhenraat <e.henraat@gmail.com>
2016-10-03 13:02:44 +0200

    added parse instructions

--- voorbeeldcode/les2/Assignment.java
+++ voorbeeldcode/les2/Assignment.java
@@ -5,6 +5,7 @@ public class Assignment{
 
 
     System.out.println("Assignment 3: Use the Scanner datatype to allow a user to enter the values for name, age, and postal code. ");
+    System.out.println("Assignment 4: Parse the Scanner output to an int when entering the age value. This way the user will get an error if numbers arent used. ");
     System.out.println("Note: Find out about datatypes at the Application Programming Interface(API) reference https://docs.oracle.com/javase/7/docs/api/allclasses-noframe.html. Select the Scanner link.");
   }
 }
--- voorbeeldcode/les2/Les2.java
+++ voorbeeldcode/les2/Les2.java
@@ -23,6 +23,10 @@ public class Les2
     Integer iw = new Integer(i);
     System.out.println("like finding the maximum range of an integer : "+ iw.MAX_VALUE);
 
+    System.out.println("you can also 'parse' a value of one datatype to another. Like prsing a String to an int");
+    int parsedInt = Integer.parseInt("1");//will parse the string value "1" into an int value;
+    String parsedString = Integer.toString(12);//will parse the int value of 12 into a String;
+
     System.out.println("examples of a reference variable, these are always stored on the HEAP");
     String firstName = "erwin";
     String lastName = new String("Henraat");
