83cab1d398aebe31148e1c8b576130c2f184a0ba
erwinhenraat <e.henraat@gmail.com>
2016-10-06 18:07:27 +0200

    les 3 gemaakt

---
+++ voorbeeldcode/les3/Les3.java
@@ -0,0 +1,52 @@
+public class Les3
+{
+  public static void main(String[] args)
+  {
+    //Opdracht 1
+    //maak een aantal variabelen aan
+    //voornaam, achternaam, leeftijd, gewicht, geslacht.
+    //vul waarden voor deze variabelen in
+
+
+    //Opdracht 2
+    //maak van het Alcohol datatype een flesbier aan
+    //maak een if statement om te kijken of je alcohol mag drinken
+    //roep de method flesbier.drink() aan als je oud genoeg bent
+
+    //Opdracht 3
+    //Drink de fles bier leeg!
+    //met de method empty() van het datatype Alcohol kun je checken of de fles leeg is
+    //deze method geeft met behulp van een boolean aan of de fles leeg is of niet
+    //Op de plek waar je deze method aanroept komt dus true of false te staan
+    //Je mag maar maximaal 1x de drink() method aanroepen.
+
+    //Opdracht 4
+    //Drink nu nog eens een slok bier om te kijken of de fles echt leeg is.
+
+    //Opdracht 5
+    //De bier is op.
+    //Gelukkig kun je nog meer drinken
+    //Maak eens een fles Whiskey en drink daar precies 3 slokken uit.
+    //gebruik hiervoor niet 3x de zelfde regel code
+
+    //Opdracht 6
+    //Je hebt nu redelijk wat code geschreven
+    //Alle code die je hebt zorgt ervoor dat je je bezat
+    //Zorg er nu voor dat al deze code alleen werkt als je ouder bent dan 18
+    //verander je leeftijd eens naar 17
+    //kun je nog drinken?
+    //Geef op de command line een boodschap dat je te jong bent om te drinken
+
+    //Opdracht 7
+    //reken uit hoeveel 243 maal 632 is. Stuur het antwoord naar de commandline
+
+    //Opdracht 8
+    //reken uit hoeveel 243 min 632 plus 549 is. Stuur het antwoord naar de commandline
+
+    //Opdracht 9
+    //reken uit wat de restwaarde van 243 gedeeld door 34 is. Stuur het antwoord naar de commandline
+
+
+  }
+
+}
