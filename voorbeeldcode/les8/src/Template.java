67e3533a24dc56b07d944ac2505f285c2b8e4abc
erwinhenraat <e.henraat@gmail.com>
2016-12-03 01:08:06 +0100

    Les8 done

--- voorbeeldcode/les8/src/Template.java
+++ voorbeeldcode/les8/src/Template.java
@@ -1,3 +1,4 @@
+//Importeren van gebruikte classes
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -7,18 +8,18 @@ import javafx.scene.image.ImageView;
 import javafx.scene.image.Image;
 public class Template extends Application{  //zorg dat de class overerft van Application
   public void start(Stage stage){   //geef de stage mee als argument
-
     //creeer de "root node" Group
     Group rootNode = new Group();
-    //Maak een nieuwe Scene met daarin de "root node"
+    //Maak een nieuwe Scene met daarin de "root node" Group (verzameling nodes)
     Scene scene = new Scene(rootNode, 800, 600);
     //plaats de Scene op de Stage
     stage.setScene(scene);
 
-    //Inladen van een Image in een ImageView
+    //inladen van een Image in een ImageView
     Image image = new Image("http://www.zombieplace.com/zombiecats/your-child-watched-too-many-zombie-movies.jpg", true);
     ImageView iv = new ImageView();
     iv.setImage(image);
+
     //De ImageView toevoegen aan de rootNode Group
     rootNode.getChildren().add(iv);
 
@@ -29,9 +30,47 @@ public class Template extends Application{  //zorg dat de class overerft van App
 
     //maak een button aan en plaats deze in de extra group
     Button button = new Button("ik ben een Button");
+    button.relocate(50,50);
     group1.getChildren().add(button);
 
     //laat de stage en inhoud renderen met de show() method
     stage.show();
+
+    //Opdracht 0
+    //Lees in de javafx API reference wat alle geimporteerde classes doen.
+    //Stel vragen als je iets niet begrijpt.
+
+    //Opdracht 1
+    //Wat gebeurt er als je de ImageView iv aan de group1 Group toevoegt,
+    //en je de button toevoegt aan de rootNode Group?
+    //Probeer het uit en verklaar wat er gebeurt en waarom dat zo is...
+    //Zet je verklaring in een comment hieronder...
+    //
+    //
+    //
+
+    //Opdracht 2
+    //Zoek uit in de javafx API reference naar de class TextField
+    //Kijk in welke package de class zit en importeer deze
+    //Maak in de start method een textField aan en plaats deze in de group1 Group
+    //Gebruik de relocate() method om het textveld netjes onder de knop te plaatsen
+
+    //Opdracht 3
+    //Kies nog een ongebruikt type node uit en plaats deze in het scherm onder het TextField
+
+    //Opdracht 4
+    //Teken een cirkel achter je knop en je textField
+
+    //Opdracht 5
+    //Lees de volgende tutorial door en laat de cirkel stuiteren tussen de onder- en bovenkant van je scherm
+    //Tutorial: http://docs.oracle.com/javase/8/javafx/scene-graph-tutorial/scenegraph.htm#JFXSG107
+
+    //Extra Opdracht 6
+    //Zorg dat de cirkel tijdens het stuiteren van kleur verandert
+
+    //Extra Opdracht 7
+    //Voor een 10!
+    //Zie de uitleg in de presentatie
+
   }
 }
